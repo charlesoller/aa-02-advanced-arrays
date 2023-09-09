40d3565daea08fa01bc45a7559b5d98a1736bbb9
Javier Hernandez <javierLhernandez@yahoo.com>
2023-09-09 18:18:50 -0400

    problem 8 DONE

--- problems/08-product-reduce.js
+++ problems/08-product-reduce.js
@@ -17,8 +17,10 @@ console.log(productWithReduce([4, 3])); // 12
 
 let productWithReduce = function(nums) {
     // Your code here 
+    return nums.reduce((product, num) => product *= num)
 }
-
+console.log(productWithReduce([10, 3, 5, 2])); // 300
+console.log(productWithReduce([4, 3])); // 12
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
